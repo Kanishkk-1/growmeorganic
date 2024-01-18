84b264041503f6d69c47976ea5e2c1038f8f1e6f
Kanishk <kanishka.ranjan.9@gmail.com>
2024-01-18 21:44:01 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+import React from "react";
+// import { useHistory } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1>ihvhi</h1>
+
+      <form action="">
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+      </form>
+    </>
+  );
+}
+
+export default App;
