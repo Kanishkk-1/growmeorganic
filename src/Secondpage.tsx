31eb2405f548b296dff844f526fce60cf36bf814
Kanishk <kanishka.ranjan.9@gmail.com>
2024-01-19 03:44:50 +0530

    datagridcheck

--- src/Secondpage.tsx
+++ src/Secondpage.tsx
@@ -1,15 +1,74 @@
-import React, { useEffect} from "react";
-import { useHistory} from "react-router-use-history";
-const Secondpage = () => {
- 
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Container,
+} from "@mui/material";
 
-
-
-  return (
-    <div>HHHHHHHHHHHHHHIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
-    
-    </div>
-  )
+interface Data {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 }
 
-export default Secondpage
+const DataList: React.FC = () => {
+  const [dataList, setDataList] = useState<Data[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        setDataList(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <h1>Data List</h1>
+      <Box sx={{ height: "50vh", width: "100%", overflowY: "auto"}}>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Id</TableCell>
+                <TableCell>Title</TableCell>
+                <TableCell>Body</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {dataList.map((data) => (
+                <TableRow>
+                  <TableCell>{data.id}</TableCell>
+                  <TableCell>{data.title}</TableCell>
+                  <TableCell>{data.body}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+
+
+
+
+      
+    </Container>
+  );
+};
+
+export default DataList;
