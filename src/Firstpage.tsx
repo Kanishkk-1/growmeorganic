a1ffda83f6f167f71c3964f2547020caaa1925b9
Kanishk <kanishka.ranjan.9@gmail.com>
2024-01-19 00:47:49 +0530

    second commit

---
+++ src/Firstpage.tsx
@@ -0,0 +1,68 @@
+import React, { useRef } from "react";
+import { useHistory } from "react-router-use-history";
+import { useState } from "react";
+
+const Firstpage: React.FC = () => {
+  const history = useHistory();
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const phoneNumberRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleClick = () => {
+    if (nameRef.current && phoneNumberRef.current && emailRef.current) {
+      const name = nameRef.current.value;
+      const phoneNumber = phoneNumberRef.current.value;
+      const email = emailRef.current.value;
+
+  if (name && phoneNumber && email) {
+        // Save data to localStorage
+        localStorage.setItem('userData', JSON.stringify({ name, phoneNumber, email}));
+
+        // Redirect to the second page
+        history.push('/second-page');
+      } else {
+        errorMessage('Please enter all details before proceeding.');
+      }
+    }
+    };
+
+  
+
+  console.log(localStorage.getItem("inputValue"), "****");
+
+  return (
+    <div>
+      <form action="">
+        <label>Name:</label>
+        <input ref={nameRef} type="text" id="name" name="name" required />
+
+        <br />
+
+        <label>Phone Number:</label>
+        <input
+          ref={phoneNumberRef}
+          type="tel"
+          id="phoneNumber"
+          name="phoneNumber"
+          required
+        />
+
+        <br />
+
+        <label>Email:</label>
+        <input ref={emailRef} type="email" id="email" name="email" required />
+
+        <br />
+
+        <button onClick={handleClick} type="button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Firstpage;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Firstpage from './Firstpage'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+// import Secondpage from './Secondpage'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Firstpage/>
+    {/* <Secondpage/> */}
   </React.StrictMode>,
 )
