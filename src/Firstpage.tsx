3d37b1b1d4cbeb269fd6fbbe784a4957e1f10036
Kanishk <kanishka.ranjan.9@gmail.com>
2024-01-19 01:46:28 +0530

    routing-sorted

--- src/Firstpage.tsx
+++ src/Firstpage.tsx
@@ -1,6 +1,7 @@
-import React, { useRef } from "react";
-import { useHistory } from "react-router-use-history";
-import { useState } from "react";
+import React, { useRef, useState ,useEffect} from "react";
+import { useHistory} from "react-router-use-history";
+import { Link } from "react-router-dom";
+
 
 const Firstpage: React.FC = () => {
   const history = useHistory();
@@ -9,31 +10,42 @@ const Firstpage: React.FC = () => {
   const phoneNumberRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
 
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  // const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-  const handleClick = () => {
+    const handleClick = () => {
     if (nameRef.current && phoneNumberRef.current && emailRef.current) {
-      const name = nameRef.current.value;
-      const phoneNumber = phoneNumberRef.current.value;
-      const email = emailRef.current.value;
-
-      if (name && phoneNumber && email) {
-        // Save data to localStorage
-        localStorage.setItem(
-          "userData",
-          JSON.stringify({ name, phoneNumber, email })
-        );
-
-        // Redirect to the second page
-        history.push("/second-page");
-      } else {
-        setErrorMessage("Please enter all details before proceeding.");
-      }
+      console.log(nameRef.current.value, phoneNumberRef.current.value, emailRef.current.value);
+      localStorage.setItem("inputValue", nameRef.current.value);
     }
   };
 
-  // console.log(localStorage.getItem("inputValue"), "****");
 
+       useEffect(() => {
+         // Check if user details are present in localStorage
+         const inputValue = localStorage.getItem("inputValue");
+
+         if (!inputValue) {
+           // If user details are not present, redirect to the first page with a message
+           alert("Please enter your details before accessing the second page.");
+           history.push("/");
+         }
+       }, [history]);
+
+      // if (name && phoneNumber && email) {    
+      //   // Save data to localStorage
+      //   localStorage.setItem(
+      //     "userData",
+      //     JSON.stringify({ name, phoneNumber, email })
+      //   );
+
+      //   // Redirect to the second page
+      //   history.push("/second-page");
+      // } else {
+
+
+        // setErrorMessage("Please enter all details before proceeding.");
+      
+    
   return (
     <div>
       <form action="">
@@ -44,7 +56,12 @@ const Firstpage: React.FC = () => {
 
         <label>Phone Number:</label>
         <input
-          ref={phoneNumberRef} type="tel" id="phoneNumber" name="phoneNumber" required />
+          ref={phoneNumberRef}
+          type="tel"
+          id="phoneNumber"
+          name="phoneNumber"
+          required
+        />
 
         <br />
 
@@ -52,11 +69,14 @@ const Firstpage: React.FC = () => {
         <input ref={emailRef} type="email" id="email" name="email" required />
 
         <br />
-
-        <button onClick={handleClick} type="button">
-          Submit
-        </button>
+        <Link to="/second-page">
+          <button onClick={handleClick} type="button">
+            Submit
+          </button>
+        </Link>
       </form>
+
+      {/* {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>} */}
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Firstpage from './Firstpage'
-import { BrowserRouter , Router , Route } from 'react-router-dom'
+import { BrowserRouter  , Route, Routes } from 'react-router-dom'
 import Secondpage from './Secondpage'
 import './index.css'
 
@@ -9,10 +9,10 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Router>
+      <Routes>
         <Route path="/" element={<Firstpage />} />
         <Route path="/second-page" element={<Secondpage />} />
-      </Router>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
