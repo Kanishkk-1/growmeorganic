be1a133b651fa0bdfd7b32f8be76ca30ebff98a5
Kanishk <kanishka.ranjan.9@gmail.com>
2024-01-19 06:23:16 +0530

    W

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import * as React from "react";
-import * as  ReactDOM from "react-dom";
-import Firstpage from "./Firstpage";
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Secondpage from "./Secondpage";
-
+import* as  React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Firstpage />} />
-        <Route path="/second-page" element={<Secondpage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
